8785410368f41f0da0f94c640c6ea140160c63a1
William Kirk <30280510+wkirk01@users.noreply.github.com>
2020-10-15 16:28:35 -0500

    Consolidating sorted algorithms

--- sortingalgorithms/insertionsort.go
+++ sortingalgorithms/insertionsort.go
@@ -6,8 +6,9 @@ import (
 
 func main() {
 	array := []int{5, 4, 3, 2, 1}
+	fmt.Println("Unsorted array", array)
 	sortedArray := BubbleSort(array)
-	fmt.Println(sortedArray)
+	fmt.Println("Sorted array", sortedArray)
 }
 
 //BubbleSort ...
@@ -18,6 +19,17 @@ func BubbleSort(array []int) []int {
 			if array[j] > array[j+1] {
 				array[j], array[j+1] = array[j+1], array[j]
 			}
+		}
+	}
+	return array
+}
+
+//InsertionSort ...
+func InsertionSort(array []int) []int {
+	length := len(array)
+	for i := 1; i < length; i++ {
+		for j := i; j > 0 && array[j] < array[j-1]; j-- {
+			array[j], array[j-1] = array[j-1], array[j]
 		}
 	}
 	return array
